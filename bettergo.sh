e3bb56aa92454f44556c68d1732c46cbcebc1900
Nathan Osman <nathan.osman@gmail.com>
2017-09-07 23:56:14 -0700

    Replace ugly commands.

--- bettergo.sh
+++ bettergo.sh
@@ -2,15 +2,12 @@
 
 USER=tmpuser
 
-# Add an entry to /etc/passwd
-if [ $UID -ne 0 ]; then
-    DAYS=`expr $(date +%s) / 86400`
-    echo "$USER:x:$UID:${GID:-$UID}::/:/bin/bash" >> /etc/passwd
-    echo "$USER:!:$DAYS::::::" >> /etc/shadow
-fi
+# Create a new group and user with the correct values
+groupadd -g $GID $USER
+useradd -mu $UID -g $GID $USER
 
 # Ensure /go is owned by the user
 chown -R $UID:$UID /go
 
 # Switch to the specified user's account and run the command
-sudo -Eu $USER env "PATH=$PATH" "$@"
+sudo -EHu $USER env "PATH=$PATH" "$@"
